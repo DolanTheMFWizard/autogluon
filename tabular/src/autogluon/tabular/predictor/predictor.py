c9522be664c1556de94bdd6be87acfaaeff47b0f
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-09-15 19:50:12 -0700

    Update predictor.py

--- tabular/src/autogluon/tabular/predictor/predictor.py
+++ tabular/src/autogluon/tabular/predictor/predictor.py
@@ -282,7 +282,7 @@ class TabularPredictor:
         if init_kwargs is None:
             init_kwargs = dict()
 
-        self.fit(train_data, **fit_kwargs)
+        self.fit(train_data=train_data, tuning_data=validation_data, **fit_kwargs)
         best_model = self
         y_pred_proba_og = self.predict_proba(test_data)
         y_pred_og = self.predict(test_data)
@@ -290,59 +290,59 @@ class TabularPredictor:
         y_pred = y_pred_og.copy()
         y_pred_proba = y_pred_proba_og.copy()
         y_pred_proba_holdout = y_pred_proba.copy()
-         
-        # if force_pseudo:
-        #     previous_score = float('-inf')
-        # else:
-        #     previous_score = self.info()['best_model_score_val']
-        #
-        # for i in range(max_iter):
-        #     # Finds pseudo labeling rows that are above threshold
-        #     test_pseudo_indices_true = self.filter_pseudo(y_pred_proba_holdout, problem_type=self.problem_type,
-        #                                                   threshold=threshold)
-        #     test_pseudo_indices = pd.Series(data=False, index=y_pred_proba_holdout.index)
-        #     test_pseudo_indices[test_pseudo_indices_true.index] = True
-        #
-        #     # Copy test data and impute labels then select indices that are above threshold
-        #     test_data_pseudo = test_data.copy()
-        #     test_data_pseudo[self.label] = y_pred
-        #     test_data_pseudo = test_data_pseudo.loc[test_pseudo_indices_true.index]
-        #
-        #     if len(test_data_pseudo) > 0:
-        #         curr_train_data = pd.concat([train_data, test_data_pseudo], ignore_index=True)
-        #         # test_data_holdout is data that should not be added into train because didn't meet threshold
-        #         test_data_holdout = test_data.copy()
-        #         test_data_holdout = test_data_holdout.loc[test_pseudo_indices[~test_pseudo_indices].index]
-        #         # predictor_pseudo = TabularPredictor(label=label, **init_kwargs).fit(train_data = train_data, **fit_kwargs)
-        #         predictor_pseudo = TabularPredictor(label=self.label, **init_kwargs).fit(train_data=curr_train_data,
-        #                                                                                  tuning_data=validation_data,
-        #                                                                                  **fit_kwargs)
-        #         curr_score = predictor_pseudo.info()['best_model_score_val']
-        #
-        #         if curr_score > previous_score:
-        #             previous_score = curr_score
-        #             best_model = predictor_pseudo
-        #         else:
-        #             break
-        #
-        #         y_pred_proba_holdout = predictor_pseudo.predict_proba(test_data_holdout)
-        #         # Sets predicted probs for heldout data
-        #         y_pred_proba.loc[test_data_holdout.index] = y_pred_proba_holdout
-        #         y_pred = predictor_pseudo.predict(test_data)
-        #
-        #         # No repeat runs on pseudo-labeled test
-        #         if reuse_pred_test:
-        #             test_data = test_data.loc[~test_pseudo_indices]
-        #             unittest.TestCase.assertTrue(
-        #                 np.all(test_data.index == test_pseudo_indices[~test_pseudo_indices].index),
-        #                 'Test data indices and pseudo labels do not match')
-        #
-        #         #######
-        #         # score_ps = predictor_pseudo.evaluate_predictions(y_true=test_data[label], y_pred=y_pred_proba)
-        #         # print(f'score_ps {i}: {score_ps}')
-        #         #######
-        #     else:
-        #         break
+
+        if force_pseudo:
+            previous_score = float('-inf')
+        else:
+            previous_score = self.info()['best_model_score_val']
+
+        for i in range(max_iter):
+            # Finds pseudo labeling rows that are above threshold
+            test_pseudo_indices_true = self.filter_pseudo(y_pred_proba_holdout, problem_type=self.problem_type,
+                                                          threshold=threshold)
+            test_pseudo_indices = pd.Series(data=False, index=y_pred_proba_holdout.index)
+            test_pseudo_indices[test_pseudo_indices_true.index] = True
+
+            # Copy test data and impute labels then select indices that are above threshold
+            test_data_pseudo = test_data.copy()
+            test_data_pseudo[self.label] = y_pred
+            test_data_pseudo = test_data_pseudo.loc[test_pseudo_indices_true.index]
+
+            if len(test_data_pseudo) > 0:
+                curr_train_data = pd.concat([train_data, test_data_pseudo], ignore_index=True)
+                # test_data_holdout is data that should not be added into train because didn't meet threshold
+                test_data_holdout = test_data.copy()
+                test_data_holdout = test_data_holdout.loc[test_pseudo_indices[~test_pseudo_indices].index]
+                # predictor_pseudo = TabularPredictor(label=label, **init_kwargs).fit(train_data = train_data, **fit_kwargs)
+                predictor_pseudo = TabularPredictor(label=self.label, **init_kwargs).fit(train_data=curr_train_data,
+                                                                                         tuning_data=validation_data,
+                                                                                         **fit_kwargs)
+                curr_score = predictor_pseudo.info()['best_model_score_val']
+
+                if curr_score > previous_score:
+                    previous_score = curr_score
+                    best_model = predictor_pseudo
+                else:
+                    break
+
+                y_pred_proba_holdout = predictor_pseudo.predict_proba(test_data_holdout)
+                # Sets predicted probs for heldout data
+                y_pred_proba.loc[test_data_holdout.index] = y_pred_proba_holdout
+                y_pred = predictor_pseudo.predict(test_data)
+
+                # No repeat runs on pseudo-labeled test
+                if reuse_pred_test:
+                    test_data = test_data.loc[~test_pseudo_indices]
+                    unittest.TestCase.assertTrue(
+                        np.all(test_data.index == test_pseudo_indices[~test_pseudo_indices].index),
+                        'Test data indices and pseudo labels do not match')
+
+                #######
+                # score_ps = predictor_pseudo.evaluate_predictions(y_true=test_data[label], y_pred=y_pred_proba)
+                # print(f'score_ps {i}: {score_ps}')
+                #######
+            else:
+                break
 
         return best_model, y_pred_proba
 
