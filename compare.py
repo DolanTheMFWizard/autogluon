d4b2ad568255a754f5408b26a406f8bbe112a2ea
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-09-30 02:22:07 -0700

    Update compare.py

--- compare.py
+++ compare.py
@@ -11,10 +11,21 @@ def run(path, metric):
     score_sums = None
     num_iter_sums = None
 
+    model_list = list(df['model'].unique())
+    model_wins_dict = dict()
+    for m in model_list:
+        model_wins_dict[m] = 0
+
+
     for id in open_ml_ids:
         df_openml = df[df.openml_id == id]
         ranks = df_openml[metric].rank(axis=0, method='average', ascending=False).to_numpy()
         scores = df_openml[metric].to_numpy()
+        models = df_openml['model'].to_numpy()
+        model_winner = models[ranks.argmin()]
+
+        model_wins_dict[model_winner] += 1
+
         num_iter = df_openml['max_num_ter'].to_numpy()
 
         if id == open_ml_ids[0]:
@@ -34,6 +45,8 @@ def run(path, metric):
     print(score_sums/num_openml_ids)
     print('Average number of iterations:')
     print(num_iter_sums/num_openml_ids)
+    print('Models 1st place counts:')
+    print(model_wins_dict)
     print('Open ML Ids:')
     print(open_ml_ids)
 
