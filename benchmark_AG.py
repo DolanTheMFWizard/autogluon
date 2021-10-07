363871c4f36b4f1f2fd893daeec8193555403759
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-10-06 21:16:23 -0700

    Update benchmark_AG.py

--- benchmark_AG.py
+++ benchmark_AG.py
@@ -151,7 +151,7 @@ def balance_pseudo(y_pred_proba_og: pd.DataFrame, X_test: pd.DataFrame, y_label:
     X[predictor.label] = y_pred
     pseudo_indices = pd.Series(data=False, index=X_test.index)
     pseudo_indices.loc[y_pred.index] = True
-    PL_predictor = TabularPredictor(label=predictor.label, eval_metric=eval_metric).fit(train_data=X)
+    PL_predictor = TabularPredictor(label=predictor.label, eval_metric=predictor.eval_metric).fit(train_data=X)
     train_data = X_test.loc[pseudo_indices[pseudo_indices == False].index]
 
     if len(train_data) == 0:
@@ -175,7 +175,7 @@ def balance_pseudo_no_holdouts(y_pred_proba_og: pd.DataFrame, X_test: pd.DataFra
     X[predictor.label] = y_pred
     pseudo_indices = pd.Series(data=False, index=X_test.index)
     pseudo_indices.loc[y_pred.index] = True
-    PL_predictor = TabularPredictor(label=predictor.label, eval_metric=eval_metric).fit(train_data=X)
+    PL_predictor = TabularPredictor(label=predictor.label, eval_metric=predictor.eval_metric).fit(train_data=X)
     y_pred_proba = PL_predictor.predict_proba(data=X_test)
 
     return y_pred_proba, y_pred_proba.idxmax(axis=1), PL_predictor
