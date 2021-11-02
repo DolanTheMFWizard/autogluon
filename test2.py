93b653282fc325e0de4ca69fc66f4a50bf2d31b5
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-11-01 23:18:40 -0700

    mix-up in progress

--- test2.py
+++ test2.py
@@ -30,13 +30,13 @@ train_data[label] = y
 numerical_features = train_data.columns[train_data.dtypes != 'category']
 categorical_features = train_data.columns[train_data.dtypes == 'category']
 
-for feat in categorical_features:
-    train_data[feat] = pd.to_numeric(train_data[feat])
-
-for feat in categorical_features:
-    X_unlabeled[feat] = pd.to_numeric(X_unlabeled[feat])
-
 if predictor_og.problem_type == 'regression':
+    for feat in categorical_features:
+        train_data[feat] = pd.to_numeric(train_data[feat])
+
+    for feat in categorical_features:
+        X_unlabeled[feat] = pd.to_numeric(X_unlabeled[feat])
+
     num_samples = int(len(train_data) / 2)
     train_sample_1 = train_data.sample(num_samples).reset_index(drop=True)
     train_sample_2 = train_data.sample(num_samples).reset_index(drop=True)
@@ -46,16 +46,31 @@ if predictor_og.problem_type == 'regression':
 
     train_data.append(train_data_mixed).reset_index(drop=True)
 else:
-    num_samples = int(len(train_data) / 4)
-    train_sample_1 = train_data.sample(num_samples).reset_index(drop=True)
-    train_sample_2 = train_data.sample(num_samples).reset_index(drop=True)
-    lam = np.ones(train_sample_1.shape) * 0.5
+    numerical_features = train_data.columns[train_data.dtypes != 'category']
+    categorical_features = train_data.columns[train_data.dtypes == 'category']
 
-    train_data_mixed = lam * train_sample_1 + (1 - lam) * train_sample_2
-    train_data_mixed[label] = train_sample_1[label]
-    train_data.append(train_data_mixed).reset_index(drop=True)
-    train_data_mixed[label] = train_sample_2[label]
-    train_data.append(train_data_mixed).reset_index(drop=True)
+    if not categorical_features.empty:
+        train_data_mixed = None
+        for index, row in train_data.iterrows():
+            train_data_subset = train_data.drop(index=index, axis='rows')
+            row_equal_cate = (row[categorical_features] == train_data_subset[categorical_features]).all(
+                axis='columns').copy()
+            row_eq_true = row_equal_cate[row_equal_cate]
+
+            if row_eq_true.empty:
+                continue
+            else:
+                sampled_rows = train_data.loc[row_eq_true.index]
+                lam = np.random.beta(0.4, 0.4, len(sampled_rows))[:, None].repeat(len(numerical_features), axis=1)
+                x_curr_mixed = lam * sampled_rows[numerical_features] + (1 - lam) * row[numerical_features].repeat(len(sampled_rows), axis=0)
+
+                if train_data_mixed is not None:
+                    train_data_mixed = train_data_mixed.append(x_curr_mixed, ignore_index=True)
+                else:
+                    train_data_mixed = x_curr_mixed
+
+        train_data_mixed = train_data_mixed.drop_duplicates()
+        train_data.append(train_data_mixed).reset_index(drop=True)
 
 predictor = TabularPredictor(label=label).fit(train_data)
 y_pred = predictor.predict_proba(X_unlabeled)
