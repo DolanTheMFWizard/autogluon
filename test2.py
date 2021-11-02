6db1c302e37ec9e32dc6148dc741f26a3417d970
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-11-01 20:23:40 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from autogluon.core.constants import REGRESSION
+from autogluon.core.constants import BINARY
 from autogluon.tabular import TabularPredictor
 
 from tabular.tests.unittests.test_tabular import load_data
@@ -8,11 +8,10 @@ from tabular.tests.unittests.test_tabular import load_data
 train_file = 'train_data.csv'
 test_file = 'test_data.csv'
 directory_prefix = './datasets/'
-dataset = {'url': 'https://autogluon.s3.amazonaws.com/datasets/AmesHousingPriceRegression.zip',
-           'name': 'AmesHousingPriceRegression',
-           'problem_type': REGRESSION,
-           'label': 'SalePrice',
-           'performance_val': 0.076}
+dataset = {'url': 'https://autogluon.s3.amazonaws.com/datasets/AdultIncomeBinaryClassification.zip',
+           'name': 'AdultIncomeBinaryClassification',
+           'problem_type': BINARY,
+           'label': 'class'}
 label = dataset['label']
 
 train_data, test_data = load_data(directory_prefix=directory_prefix, train_file=train_file, test_file=test_file,
@@ -37,14 +36,26 @@ for feat in categorical_features:
 for feat in categorical_features:
     X_unlabeled[feat] = pd.to_numeric(X_unlabeled[feat])
 
-num_samples = int(len(train_data)/2)
-train_sample_1 = train_data.sample(num_samples).reset_index(drop=True)
-train_sample_2 = train_data.sample(num_samples).reset_index(drop=True)
-lam = np.random.beta(0.4, 0.4, num_samples)[:, None].repeat(len(train_data.columns), axis=1)
+if predictor.problem_type == 'regression':
+    num_samples = int(len(train_data) / 2)
+    train_sample_1 = train_data.sample(num_samples).reset_index(drop=True)
+    train_sample_2 = train_data.sample(num_samples).reset_index(drop=True)
+    lam = np.random.beta(0.4, 0.4, num_samples)[:, None].repeat(len(train_data.columns), axis=1)
 
-train_data_mixed = lam * train_sample_1 + (1 - lam) * train_sample_2
+    train_data_mixed = lam * train_sample_1 + (1 - lam) * train_sample_2
 
-train_data.append(train_data_mixed).reset_index(drop=True)
+    train_data.append(train_data_mixed).reset_index(drop=True)
+else:
+    num_samples = int(len(train_data) / 4)
+    train_sample_1 = train_data.sample(num_samples).reset_index(drop=True)
+    train_sample_2 = train_data.sample(num_samples).reset_index(drop=True)
+    lam = np.ones(train_sample_1.shape) * 0.5
+
+    train_data_mixed = lam * train_sample_1 + (1 - lam) * train_sample_2
+    train_data_mixed[label] = train_sample_1[label]
+    train_data.append(train_data_mixed).reset_index(drop=True)
+    train_data_mixed[label] = train_sample_2[label]
+    train_data.append(train_data_mixed).reset_index(drop=True)
 
 predictor = TabularPredictor(label=label).fit(train_data)
 predictor.predict(X_unlabeled)
