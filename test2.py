5336cc524437fb759f255c5c1e074f463c5e23db
DolanTheMFWizard <89041085+DolanTheMFWizard@users.noreply.github.com>
2021-11-03 17:10:38 -0700

    smote added

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from imblearn.over_sampling import BorderlineSMOTE
 from autogluon.core.constants import BINARY
 from autogluon.tabular import TabularPredictor
 
@@ -46,37 +47,12 @@ if predictor_og.problem_type == 'regression':
 
     train_data.append(train_data_mixed).reset_index(drop=True)
 else:
-    numerical_features = train_data.columns[train_data.dtypes != 'category']
-    categorical_features = train_data.columns[train_data.dtypes == 'category']
+    X_resampled, y_resampled = BorderlineSMOTE().fit_resample(X, y)
+    resampled_df = pd.DataFrame(X_resampled, columns=X.columns)
+    resampled_df[label] = pd.Series(y_resampled)
 
-    if not categorical_features.empty:
-        grouped_df = train_data.groupby(by=list(categorical_features))
-        mixed_rows_df = None
-        for key, value in grouped_df.groups.items():
-            num_rows = len(value)
-            if num_rows < 2:
-                continue
-
-            if num_rows % 2 != 0:
-                num_rows -= 1
-
-            selected_rows = train_data.loc[value[:num_rows]]
-
-            half_num_rows = int(num_rows/2)
-
-            sample_1_df = selected_rows.iloc[:half_num_rows].reset_index(drop=True)
-            sample_2_df = selected_rows.iloc[half_num_rows:].reset_index(drop=True)
-
-            lam = np.random.beta(0.4, 0.4, half_num_rows)[:, None].repeat(len(numerical_features), axis=1)
-
-            new_mixed_rows_df = lam * sample_1_df[numerical_features] + (1 - lam) * sample_2_df[numerical_features]
-
-            if mixed_rows_df is not None:
-                mixed_rows_df = mixed_rows_df.append(new_mixed_rows_df, ignore_index=True)
-            else:
-                mixed_rows_df = new_mixed_rows_df
-
-        train_data = train_data.append(mixed_rows_df, ignore_index=True).reset_index(drop=True)
+    train_data = train_data.append(resampled_df, ignore_index=True).reset_index(drop=True)
+    print('')
 
 
 predictor = TabularPredictor(label=label).fit(train_data)
